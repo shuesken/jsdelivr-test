cb0e0a8da3d18736d434a39757cc28407261f192
Simon <32738398+shuesken@users.noreply.github.com>
2025-02-13 16:05:07 +0100

    Update and rename proxies5.js to proxies6.js

--- proxies6.js
+++ proxies6.js
@@ -2,6 +2,7 @@ function generateHandler(name) {
   return {
     apply(actualFetch, that, args) {
       console.log("intercepted", name)
+      console.log(actualFetch)
       console.log(args?.at(1)?.headers?.get('authorization'))
       const result = Reflect.apply(actualFetch, that, args);
       return result;
@@ -11,8 +12,8 @@ function generateHandler(name) {
 
 // window.fetch = new Proxy(window.fetch, generateHandler("window window"))
 // window.parent.fetch = new Proxy(window.parent.fetch, generateHandler("parent parent"))
-window.parent.fetch = new Proxy(fetch, generateHandler("parent window"))
-window.parent.parent.fetch = new Proxy(fetch, generateHandler("parent window"))
-window.parent.parent.parent.fetch = new Proxy(fetch, generateHandler("parent window"))
+window.parent.fetch = new Proxy(window.parent.fetch, generateHandler("parent parent"))
+// window.parent.parent.fetch = new Proxy(fetch, generateHandler("parent window"))
+// window.parent.parent.parent.fetch = new Proxy(fetch, generateHandler("parent window"))
 // window.fetch = new Proxy(window.parent.fetch, generateHandler("window parent"))
 
