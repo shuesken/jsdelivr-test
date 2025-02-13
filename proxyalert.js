244c239609b9d02e5dc8c592831d08a03286c295
Simon <32738398+shuesken@users.noreply.github.com>
2025-02-13 15:32:08 +0100

    Create proxyalert.js

---
+++ proxyalert.js
@@ -0,0 +1,12 @@
+function install() {
+  console.log('proxying alert in window')
+  window.alert = new Proxy(window.alert, {
+    apply(actualAlert, that, args) {
+    	console.log('intercepting alert')
+      const result = Reflect.apply(actualAlert, that, args);
+      return result;
+    }
+  );
+}
+
+setInterval(install, 1000)
