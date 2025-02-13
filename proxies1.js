8de34f6ed695f47dc4a862fd9d7dc8316c68bb3a
Simon <32738398+shuesken@users.noreply.github.com>
2025-02-13 15:46:16 +0100

    Create proxies1.js

---
+++ proxies1.js
@@ -0,0 +1,14 @@
+function generateHandler(name) {
+  return {
+    apply(actualFetch, that, args) {
+      console.log("intercepted" name)
+      const result = Reflect.apply(actualFetch, that, args);
+      return result;
+    }
+  }
+}
+
+new Proxy(window.fetch, generateHandler("window"))
+new Proxy(window.parent.fetch, generateHandler("window.parent"))
+new Proxy(window.parent.parent.fetch, generateHandler("window.parent.parent.fetch"))
+new Proxy(fetch, generateHandler("fetch"))
